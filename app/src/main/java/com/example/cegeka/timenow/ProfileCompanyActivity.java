5dd83c15ca7fc398a71872575ece3e057f62f474
Bionic12 <bionic119@yahoo.com>
2018-06-17 21:20:55 +0300

    update

--- app/src/main/java/com/example/cegeka/timenow/ProfileCompanyActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/ProfileCompanyActivity.java
@@ -1,9 +1,16 @@
 package com.example.cegeka.timenow;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileCompanyActivity extends AppCompatActivity {
 
@@ -18,6 +25,31 @@ public class ProfileCompanyActivity extends AppCompatActivity {
         NameTv = (TextView) findViewById(R.id.CmpNameTV);
         Intent intent = getIntent();
         String ID = intent.getStringExtra("ID_COMPANIE");
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference("users/"+ID);
+
+
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+               NameTv.setText(dataSnapshot.child("name").getValue(String.class));
+                PhoneTv.setText(dataSnapshot.child("phone").getValue(String.class));
+                AdressTv.setText(dataSnapshot.child("adress").getValue(String.class));
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
 
     }
 }
