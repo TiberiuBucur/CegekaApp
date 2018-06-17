ab4a01abeb7e08f803e31bb3d772f84981aee6b8
Axlxx <nichita_axl@yahoo.com>
2018-06-17 22:46:59 +0300

    Merge branch 'master' of https://github.com/TiberiuBucur/CegekaApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/cegeka/timenow/CompanyMenuActivity.java

--- app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
@@ -1,13 +1,20 @@
 package com.example.cegeka.timenow;
 
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -27,15 +34,18 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void ChangePassword(View view){
-        EditText oldPass = findViewById(R.id.editText);
-        EditText newPass = findViewById(R.id.editText1);
 
-   //     if(oldPass.getText().toString() == FirebaseAuth.getInstance().getCurrentUser().reauthenticate().toString()){
-            FirebaseAuth.getInstance().getCurrentUser().updatePassword(newPass.getText().toString());
-        //}
+        FirebaseAuth.getInstance().sendPasswordResetEmail(FirebaseAuth.getInstance().getCurrentUser().getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(SettingsActivity.this,"Email sent" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+        );
     }
 
-    public void ChangeEmail(View view){
+    public void ChangeEmail(View view) {
         EditText newEmail = findViewById(R.id.EmailText);
         FirebaseAuth.getInstance().getCurrentUser().updateEmail(newEmail.getText().toString());
     }
@@ -43,6 +53,8 @@ public class SettingsActivity extends AppCompatActivity {
     public void ChangeNumber(View view){
         EditText newNumber = findViewById(R.id.NumberText);
         FirebaseAuth.getInstance().getCurrentUser().updatePhoneNumber((PhoneAuthCredential)newNumber.getText());
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("users/" + FirebaseAuth.getInstance().getCurrentUser().getUid() + "/phone");
     }
 
     public void DoneButt(View view){
