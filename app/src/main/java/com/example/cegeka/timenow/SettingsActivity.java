a9c59a37e98af5e1361b0cac17f3cbb81c35312d
Axlxx <nichita_axl@yahoo.com>
2018-06-17 18:25:05 +0300

    Merge branch 'master' of https://github.com/TiberiuBucur/CegekaApp
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/cegeka/timenow/CompanyMenuActivity.java

--- app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
@@ -2,11 +2,16 @@ package com.example.cegeka.timenow;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,28 @@ public class SettingsActivity extends AppCompatActivity {
         TextView nrTel = findViewById(R.id.textView3);
 
         //nrTel.setText(FirebaseAuth.getInstance().getCurrentUser().getNrTel().toString());
+    }
+
+    public void ChangePassword(View view){
+        EditText oldPass = findViewById(R.id.editText);
+        EditText newPass = findViewById(R.id.editText1);
+
+   //     if(oldPass.getText().toString() == FirebaseAuth.getInstance().getCurrentUser().reauthenticate().toString()){
+            FirebaseAuth.getInstance().getCurrentUser().updatePassword(newPass.getText().toString());
+        //}
+    }
+
+    public void ChangeEmail(View view){
+        EditText newEmail = findViewById(R.id.EmailText);
+        FirebaseAuth.getInstance().getCurrentUser().updateEmail(newEmail.getText().toString());
+    }
+
+    public void ChangeNumber(View view){
+        EditText newNumber = findViewById(R.id.NumberText);
+        FirebaseAuth.getInstance().getCurrentUser().updatePhoneNumber((PhoneAuthCredential)newNumber.getText());
+    }
+
+    public void DoneButt(View view){
+        this.finish();
     }
 }
