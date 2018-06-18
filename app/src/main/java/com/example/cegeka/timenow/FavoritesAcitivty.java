570c827a739a733265da05b280889417f3bfa431
Bionic12 <bionic119@yahoo.com>
2018-06-18 19:27:37 +0300

    ya

--- app/src/main/java/com/example/cegeka/timenow/FavoritesAcitivty.java
+++ app/src/main/java/com/example/cegeka/timenow/FavoritesAcitivty.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,7 +44,7 @@ public class FavoritesAcitivty extends AppCompatActivity {
 
                 for( DataSnapshot kido : dataSnapshot.getChildren())
                 {
-                    if(kido.child("type").getValue(boolean.class))
+                    if(kido.child("type").getValue(boolean.class)&&kido.child("favorite").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).getValue(boolean.class)!=null)
                     {
                         Company c=new Company(kido.child("name").getValue(String.class),kido.getKey());
                         arraylist.add(c);
