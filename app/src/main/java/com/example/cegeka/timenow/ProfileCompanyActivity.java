8d0f72303d7f616669ba0e3a49bc23e91ced3a16
Bionic12 <bionic119@yahoo.com>
2018-06-18 19:18:34 +0300

    ya

--- app/src/main/java/com/example/cegeka/timenow/ProfileCompanyActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/ProfileCompanyActivity.java
@@ -6,8 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,8 +19,9 @@ import com.google.firebase.database.ValueEventListener;
 public class ProfileCompanyActivity extends AppCompatActivity {
 
     TextView PhoneTv, AdressTv, NameTv;
-    Button ReserveBtn;
+    Button ReserveBtn,RateBtn;
     String ID;
+    RatingBar rtb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +30,8 @@ public class ProfileCompanyActivity extends AppCompatActivity {
         AdressTv = (TextView) findViewById(R.id.AdressTV);
         NameTv = (TextView) findViewById(R.id.CmpNameTV);
         ReserveBtn = (Button) findViewById(R.id.ReservationBtn);
+        RateBtn=findViewById(R.id.rateBtn);
+        rtb= findViewById(R.id.ratingBar);
         ReserveBtn.setEnabled(false);
         Intent intent = getIntent();
         ID = intent.getStringExtra("ID_COMPANIE");
@@ -38,7 +43,9 @@ public class ProfileCompanyActivity extends AppCompatActivity {
                NameTv.setText(dataSnapshot.child("name").getValue(String.class));
                 PhoneTv.setText(dataSnapshot.child("phone").getValue(String.class));
                 AdressTv.setText(dataSnapshot.child("adress").getValue(String.class));
-               ReserveBtn.setEnabled(true);
+                if(dataSnapshot.child("rating").child("rate").getValue(float.class)!=null)
+                rtb.setRating(dataSnapshot.child("rating").child("rate").getValue(float.class));
+                ReserveBtn.setEnabled(true);
             }
 
             @Override
@@ -55,4 +62,40 @@ public class ProfileCompanyActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+public void Rate(View view)
+{
+    DatabaseReference ref= FirebaseDatabase.getInstance().getReference("users").child(ID).child("rating");
+    ref.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(rtb.getRating());
+    ref.addListenerForSingleValueEvent(new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+            float rating=3*10;
+            int nratings=10;
+            for(DataSnapshot ds:dataSnapshot.getChildren())
+            {if(ds.getKey()!="rate") {
+                rating += ds.getValue(float.class);
+                nratings++;
+                }
+
+                dataSnapshot.child("rate").getRef().setValue(rating/nratings);
+
+
+            }
+
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+
+
+
+}
+
+
 }
