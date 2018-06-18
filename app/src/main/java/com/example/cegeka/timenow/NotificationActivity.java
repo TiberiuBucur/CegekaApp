fe2b1a2f99fb44f47e5b2b8d94f35ff542ad5126
TiberiuBucur <tbucur7000@yahoo.com>
2018-06-18 14:36:00 +0300

    Notificari client

--- app/src/main/java/com/example/cegeka/timenow/NotificationActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/NotificationActivity.java
@@ -1,35 +1,89 @@
 package com.example.cegeka.timenow;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
 public class NotificationActivity extends AppCompatActivity {
 
-    ListView NoteListView = null;
-    List<String> adapterList;
-    static int hourms = 3600000;
+    ListView NoteLv = (ListView) findViewById(R.id.NoteListView);
+    Button deleteBtn;
+    ArrayList<String> arraylist = new ArrayList<>();
+    CustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        deleteBtn = (Button) findViewById(R.id.DeleteBtn);
+        adapter = new CustomAdapter(NotificationActivity.this, arraylist);
+        NoteLv.setAdapter(adapter);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.DeleteSelected();
+                adapter = new CustomAdapter(NotificationActivity.this, arraylist);
+                NoteLv.setAdapter(adapter);
+            }
+        });
 
-        ListView NoteListView = (ListView) findViewById(R.id.NoteListView);
-        adapterList = new ArrayList<>();
-        ArrayList<Notification> lista = null;
+    }
+    public class CustomAdapter extends BaseAdapter
+    {
+        Context mContext;
+        List<CheckedTextView> List = new ArrayList<>();
+        ArrayList<String> arrayList = new ArrayList<>();
+        public CustomAdapter(Context context, ArrayList<String> List)
+        {
+            mContext = context;
+            this.arrayList = List;
+        }
+        @Override
+        public int getCount() {
+            return arrayList.size();
+        }
 
-        long crt_timestamp = System.currentTimeMillis();
-        if(lista != null)
-            for(Notification item : lista)
-                if(item.timestamp + hourms>crt_timestamp)
-                    adapterList.add(item.Message);
+        @Override
+        public Object getItem(int position) {
+            return arrayList.get(position);
+        }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(NotificationActivity.this,
-                R.layout.note_msg,R.id.NoteTV, adapterList);
-        NoteListView.setAdapter(adapter);
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if(view == null)
+            {
+                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.note_msg, null);
+            }
+            CheckedTextView Notification = (CheckedTextView) view.findViewById(R.id.NoteCheckedTV);
+            Notification.setText(arrayList.get(position));
+            List.add(Notification);
+            return view;
+        }
+        public void DeleteSelected()
+        {
+            for(CheckedTextView a : List)
+                if(a.isChecked())
+                {
+                    List.remove(a);
+                }
+        }
     }
 }
