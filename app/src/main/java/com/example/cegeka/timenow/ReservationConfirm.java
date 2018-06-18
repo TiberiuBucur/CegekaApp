41db2cf9ad91eaf8e3b83788dda686aceb9a26d6
Bionic12 <bionic119@yahoo.com>
2018-06-18 17:15:43 +0300

    ya

--- app/src/main/java/com/example/cegeka/timenow/ReservationConfirm.java
+++ app/src/main/java/com/example/cegeka/timenow/ReservationConfirm.java
@@ -1,15 +1,29 @@
 package com.example.cegeka.timenow;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+
 public class ReservationConfirm extends AppCompatActivity {
 
     TextView MessageTV;
     Button YesBtn, NoBtn;
+    String id,idu,nume,pers;
+    Date d;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +32,42 @@ public class ReservationConfirm extends AppCompatActivity {
         MessageTV = (TextView) findViewById(R.id.MessageTv);
         YesBtn = (Button) findViewById(R.id.YesBtn);
         NoBtn = (Button) findViewById(R.id.NoBtn);
+        Intent intent= getIntent();
+        intent.getStringExtra("ID");
+
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("reservations").child(id);
+ref.addListenerForSingleValueEvent(new ValueEventListener() {
+    @Override
+    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+         d=dataSnapshot.child("data").getValue(Date.class);
+        idu=dataSnapshot.child("id").getValue(String.class);
+        nume=dataSnapshot.child("nume").getValue(String.class);
+        pers=dataSnapshot.child("pers").getValue(String.class);
+
+
+
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+    }
+});
+
     }
     public void Yes(View view)
     {
+
+
+
+
         setResult(RESULT_OK);
     }
+
+
+
+
     public void No(View view)
     {
         setResult(RESULT_OK);
