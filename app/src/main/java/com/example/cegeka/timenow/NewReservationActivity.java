d67cfed65022ba65197f20394beff8e927ef212e
TiberiuBucur <tbucur7000@yahoo.com>
2018-06-18 11:25:50 +0300

    Design smecheeeerrr

--- app/src/main/java/com/example/cegeka/timenow/NewReservationActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/NewReservationActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +20,6 @@ import java.util.Locale;
 import static java.lang.System.currentTimeMillis;
 
 public class NewReservationActivity extends AppCompatActivity {
-
     TextView CmpTv;
     Button MakeResBtn;
     EditText HourEt, PersonEt, DateEt;
