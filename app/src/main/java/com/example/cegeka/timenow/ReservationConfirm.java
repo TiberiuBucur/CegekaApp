6100de8d21c9f2e9ccd38cd40cb1ff10a5c27c4e
Bionic12 <bionic119@yahoo.com>
2018-06-22 20:42:05 +0300

    Third of the last

--- app/src/main/java/com/example/cegeka/timenow/ReservationConfirm.java
+++ app/src/main/java/com/example/cegeka/timenow/ReservationConfirm.java
@@ -44,7 +44,7 @@ public class ReservationConfirm extends AppCompatActivity {
                 idu = dataSnapshot.child("id").getValue(String.class);
                 nume = dataSnapshot.child("nume").getValue(String.class);
                 pers = dataSnapshot.child("pers").getValue(Integer.class);
-                MessageTV.setText("Ati primit o rezervare de la " + nume + " pe data de " + String.valueOf(d.getDay()) + "." +
+                MessageTV.setText("Ati primit o rezervare de la " + nume + " pe data de " + String.valueOf(d.getDate()) + "." +
                         String.valueOf(d.getMonth() + 1) + "." + String.valueOf(d.getYear()+1900) + " de la ora " + String.valueOf(d.getHours()) +
                 ":" + String.valueOf(d.getMinutes()) + " pentru " + String.valueOf(pers) + " persoana/e.");
         }
@@ -58,7 +58,7 @@ public class ReservationConfirm extends AppCompatActivity {
     {
         FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("reservations").child(id).child("yes").setValue(true);
         Message = "Compania " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName() + " a acceptat rezervarea dumneavoastra de pe " +
-                String.valueOf(d.getDay()) + "." +
+                String.valueOf(d.getDate()) + "." +
                 String.valueOf(d.getMonth() +1) + "." + String.valueOf(d.getYear()+1900) + " de la ora " + String.valueOf(d.getHours()) + ":" + String.valueOf(d.getMinutes()) + " pentru " + String.valueOf(pers) + " persoana/e.";
 
         FirebaseDatabase.getInstance().getReference("users").child(idu).child("notifications").child(String.valueOf(System.currentTimeMillis())).setValue(Message);
@@ -70,7 +70,7 @@ public class ReservationConfirm extends AppCompatActivity {
     {
         FirebaseDatabase.getInstance().getReference("users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).child("reservations").child(id).removeValue();
         Message = "Compania " + FirebaseAuth.getInstance().getCurrentUser().getDisplayName() + " nu a acceptat rezervarea dumneavoastra de pe " +
-                String.valueOf(d.getDay()) + "." +
+                String.valueOf(d.getDate()) + "." +
                         String.valueOf(d.getMonth() + 1) + "." + String.valueOf(d.getYear()+1900) + " de la ora " + String.valueOf(d.getHours()) + ":" + String.valueOf(d.getMinutes()) + " pentru " + String.valueOf(pers) + " persoana/e.";
         FirebaseDatabase.getInstance().getReference("users").child(idu).child("notifications").child(String.valueOf(System.currentTimeMillis())).setValue(Message);
         startActivity(new Intent(ReservationConfirm.this, CompanyNotificationActivity.class));
--- app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
+++ app/src/main/java/com/example/cegeka/timenow/SettingsActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.database.DatabaseReference;
@@ -18,17 +19,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class SettingsActivity extends AppCompatActivity {
 
-
+TextView mparola,memail,mnumar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+memail=findViewById(R.id.EmailText);
+mnumar=findViewById(R.id.NumberText);
+mparola=findViewById(R.id.TextParola);
 
 
     }
 
     public void ChangePassword(View view){
-
         FirebaseAuth.getInstance().sendPasswordResetEmail(FirebaseAuth.getInstance().getCurrentUser().getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
             public void onComplete(@NonNull Task<Void> task) {
                 if (task.isSuccessful())
@@ -38,12 +41,24 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public void ChangeEmail(View view) {
-        FirebaseAuth.getInstance().getCurrentUser().updateEmail(FirebaseAuth.getInstance().getCurrentUser().getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
+
+
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(FirebaseAuth.getInstance().getCurrentUser().getEmail(),mparola.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(SettingsActivity.this,"Email schimbat",1).show();
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                FirebaseAuth.getInstance().getCurrentUser().updateEmail(memail.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(SettingsActivity.this,"Email schimbat",1).show();
+                    }
+                });
+
+
             }
         });
+
+
 
 
     }
